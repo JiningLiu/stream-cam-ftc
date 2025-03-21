27b1b1bf1230eb151aad337b4965bb9cf3f697ad
Jining Liu <jiningdev@gmail.com>
2025-03-21 02:18:33 -0500

    idk smth

--- src/routes/overlay/official/+page.svelte
+++ src/routes/overlay/official/+page.svelte
@@ -1,9 +1,19 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let eventCode = 'USTXTBSQ';
+
+	onMount(() => {
+		eventCode = prompt("Event code:", eventCode) || eventCode;
+	});
+</script>
+
 <head>
 	<link rel="stylesheet" href="/global.css" />
 </head>
 
 <iframe
-	src="http://localhost/event/ustxauq/display/?type=audience&bindToField=all&scoringBarLocation=bottom&allianceOrientation=standard&liveScores=true&mute=false&muteRandomizationResults=false&fieldStyleTimer=false&overlay=true&overlayColor=transparent&allianceSelectionStyle=classic&awardsStyle=overlay&dualDivisionRankingStyle=sideBySide&rankingsFontSize=larger&rankingsShowQR=false&showMeetRankings=false&rankingsAllTeams=true"
+	src="http://localhost/event/{eventCode.toLowerCase()}/display/?type=audience&bindToField=all&scoringBarLocation=bottom&allianceOrientation=standard&liveScores=true&mute=true&muteRandomizationResults=true&fieldStyleTimer=false&overlay=true&overlayColor=transparent&allianceSelectionStyle=classic&awardsStyle=overlay&dualDivisionRankingStyle=sideBySide&rankingsFontSize=larger&rankingsShowQR=false&showMeetRankings=false&rankingsAllTeams=true"
 	frameborder="0"
 	title="FTC Official Overlay"
 ></iframe>
@@ -17,6 +27,5 @@
 	iframe {
 		width: 100%;
 		height: 100%;
-		opacity: 0;
 	}
 </style>
